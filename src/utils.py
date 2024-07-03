b19c1ec8f40371aaa5a2e57d67b6aad90af915f1
sanyuan.hy <sanyuan.hy@alibaba-inc.com>
2024-07-03 11:31:14 +0800

    feature: update readme

--- src/utils.py
+++ src/utils.py
@@ -38,6 +38,7 @@ def set_seed(args):
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     if args.n_gpu > 0:
+        torch.cuda.manual_seed(args.seed)
         torch.cuda.manual_seed_all(args.seed)
 
 
