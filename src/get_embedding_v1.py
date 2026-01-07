01578a4179a9bc7f5e4a324cb1c1cfe3cf603c3b
YongHe <sanyuan.hy@alibaba-inc.com>
2026-01-07 17:04:52 +0800

    LucaOne now supports flash attention and bp16  for training

--- src/get_embedding_v1.py
+++ src/get_embedding_v1.py
@@ -973,7 +973,7 @@ def main(model_args):
                                 matrix_add_special_token=matrix_add_special_token,
                                 save_type=model_args.save_type,
                                 use_cpu=use_cpu,
-                                use_bp16=not use_cpu
+                                use_bp16=not use_cpu and model_args.use_bp16
                             )
                         if use_cpu:
                             print("use_cpu: %r" % use_cpu)
@@ -1018,7 +1018,7 @@ def main(model_args):
                                 matrix_add_special_token=matrix_add_special_token,
                                 save_type=model_args.save_type,
                                 use_cpu=use_cpu,
-                                use_bp16=not use_cpu
+                                use_bp16=not use_cpu and model_args.use_bp16
                             )
                         if use_cpu:
                             print("use_cpu: %r" % use_cpu)
@@ -1095,7 +1095,7 @@ def main(model_args):
                     matrix_add_special_token=matrix_add_special_token,
                     save_type=model_args.save_type,
                     use_cpu=use_cpu,
-                    use_bp16=not use_cpu
+                    use_bp16=not use_cpu and model_args.use_bp16
                 )
             if use_cpu:
                 print("use_cpu: %r" % use_cpu)
