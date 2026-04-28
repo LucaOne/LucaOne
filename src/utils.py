f113bfb7b9867c50ca4debd60074a0faaab362ba
YongHe <sanyuan.hy@alibaba-inc.com>
2026-04-28 18:37:08 +0800

    debug: fix the bug for getting embedding using bf16

--- src/utils.py
+++ src/utils.py
@@ -2054,6 +2054,7 @@ def calc_emb_filename_by_seq_id(seq_id, embedding_type):
     if seq_id[0] == ">":
         seq_id = seq_id[1:]
     if "|" in seq_id:
+        print(f"Warning: the seq id({seq_id}) contain invalid char '|', it will be split and selected the idx=1 as the unique id.")
         strs = seq_id.split("|")
         if len(strs) > 1:
             emb_filename = embedding_type + "_" + strs[1].strip() + ".pt"
